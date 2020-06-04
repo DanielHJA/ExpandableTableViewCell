6034fe32e8ef98aea7454548c438ee1186b4246c
Daniel Hjärtström <daniel.hjartstrom@consid.se>
2020-06-04 11:02:24 +0200

    Initial Commit

---
+++ ExpandTableViewCell2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpandTableViewCell2
+//
+//  Created by Daniel Hjärtström on 2020-06-04.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
